a5c9fbc026e450d9ff9c342a6387d139149f4ead
Oleg Kutkov <olegkutkov@yandex-team.ru>
2013-05-01 23:08:41 +0400

    example was updated

--- example.js
+++ example.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var coca_framework = require("nodejs_cocaine_framework");
+var coca_framework = require("./nodejs_cocaine_framework");
 var util = require("util");
 
 var argv=process.argv, ai={}
@@ -12,64 +12,15 @@ var options={
   uuid: argv[ai["--uuid"]+1]}
 
 console.log(argv);
-
 console.log(coca_framework)
-
-////// services ////////
-
-function on_resolve(arg)
-{
-	console.log(arg)
-
-	var storage_service = new coca_framework.ServiceStorage(arg['host'], arg['port']);
-
-	storage_service.Write("collection_name", "key_name", "buzzinga");
-	storage_service.List("collection_name");
-	storage_service.Read("collection_name", "key_name");
-	storage_service.Remove("collection_name", "key_name");
-
-	storage_service.on_storage_read_done = function on_storage_read_done(sid) {
-		console.log("read was done, sid=" + sid);
-	}
-
-	storage_service.on_storage_read = function on_storage_read(sid, data) {
-		console.log("read data=" + data + " sid=" + sid);
-	}
-
-	storage_service.on_storage_error = function on_storage_error(sid, code, msg) {
-		console.log("Storage error, session id=" 
-			+ sid 
-			+ ", error code=" 
-			+ code 
-			+ " message=" 
-			+ str);
-	}
-}
-
-function on_resolve_error(sid, code, str)
-{
-	console.log("Failed to resolve service, session id=" 
-			+ sid 
-			+ ", error code=" 
-			+ code 
-			+ " message=" 
-			+ str);
-}
-
-var service_resolver = new coca_framework.ServiceResolver("127.0.0.1", 10053);
-
-service_resolver.on_resolve = on_resolve;
-service_resolver.on_error = on_resolve_error;
-
-var storage_sid = service_resolver.ResolveByName("storage");
-
-var fake_sid = service_resolver.ResolveByName("fake");
-
-////// services ////////
 
 
 ///// worker /////////
-var worker = new coca_framework.Worker(options["endpoint"], options["uuid"]);
+var worker = new coca_framework.communicator(options["endpoint"]);
+
+worker.on_heartbeat = function on_heartbeat() {
+	console.log("heartbeat");
+}
 
 worker.on_invoke = function on_invoke(sid, method) {
 	console.log("invoke method=" + method + " sid=" + sid);
@@ -96,6 +47,30 @@ worker.on_terminate = function on_terminate() {
 process.__worker = worker;
 
 ///// worker ////////
+
+//// tcp //////
+
+var tcp = new coca_framework.communicator("127.0.0.1", 10053);
+
+tcp.on_chunk = function on_chunk(sid, data) {
+	console.log("chunk with data=" + data + " sid=" + sid);
+}
+
+tcp.on_choke = function on_choke(sid) {
+	console.log("choke session id=" + sid);
+
+	worker.Send(sid, "avada kedavra!");
+}
+
+tcp.on_error = function on_error(sid, code, msg) {
+	console.log("error in session id=" + sid + " code=" + code + " message=" + msg);
+}
+
+tcp.on_terminate = function on_terminate() {
+	console.log("terminate session");
+}
+
+//// tcp //////
 
 console.log("app %s has been started", options["app"]);
 

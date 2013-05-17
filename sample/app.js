a60b3e2bac65d21ffa97e6d4c03278a3272e1343
Dmitry Unkovsky <oil.crayons@gmail.com>
2013-05-17 13:34:16 +0300

    app that actually works

--- sample/app.js
+++ sample/app.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 
 var Q = require("q")
 var co = require("..")
@@ -5,18 +6,20 @@ var argv = require("optimist").argv
 var mp = require("msgpack")
 
 var Storage = co.Service("storage")
-var Logger = co.Service("logger")
+var Logger = co.Service("logging")
 
-var W = new co.Worker(opts)
-
-var S,L
+var W,S,L
 
 Q.all([Storage.resolve(),
        Logger.resolve()])
   .done(function(){
+    console.log("services have to be resolved")
     S = new Storage()
     L = new Logger(argv.app)
-    W.on("http",function(stream){
+    
+    var W = new co.Worker(argv)
+    W.on("hash",function(stream){
+      console.log("got http event")
       var meta
       var body = []
       var length = 0
@@ -30,14 +33,17 @@ Q.all([Storage.resolve(),
       })
       
       stream.on("end",function(){
-        stream.write(
-          mp.pack({code:200,
-                   headers:[
-                     ["content-type","text/plain"],
-                     ["x-by","worker"+argv.uuid]]}))
-        stream.write("that's who I am:\n")
+        // stream.write(
+        //   mp.pack({code:200,
+        //            headers:[
+        //              ["content-type","text/plain"],
+        //              ["x-by","worker"+argv.uuid]]}))
+        stream.write("that's who I am\n")
         var m = S.read("manifests",argv.app)
         m.on("data",function(data){
+          console.log("data",data.length,data)
+          var manifests = mp.unpack(data)
+          console.log("manifests",manifests)
           stream.write(JSON.stringify(manifests,null,2))
         })
         m.on("end",function(){

6c8c7cbaf5dcabe508ec58410b7281c2c5f64ecd
Dmitry Unkovsky <oil.crayons@gmail.com>
2013-06-20 16:14:35 +0300

    Fix proxy. Add proxy forking workers.

--- sample/proxy.js
+++ sample/proxy.js
@@ -1,19 +1,21 @@
 
 var co = require("..")
 
-var P = new co.Proxy()
+var dbg = 0
 
-P.listen(function(rq,rs0){
+var P = new co.Proxy(function(rq,rs0){
+  dbg && console.log("rq.url",rq.url)
   var chunks = [this.bakeHeader(rq)]
   var se = this.getRoute(rq.url)
   if(!se){
-    rs.writeHead(503)
-    rs.end("no service found")
+    rs0.writeHead(503)
+    rs0.end("no service found")
   } else {
     this.getApp(se[0],function(err,app){
+      dbg && console.log("err,app:",err,app)
       if(err){
-        rs.writeHead(503)
-        rs.end("no service found: "+se[0])
+        rs0.writeHead(503)
+        rs0.end("no service found: "+se[0])
       } else {
         rq.on("data",function(chunk){
           chunks.push(chunk)
@@ -26,6 +28,8 @@ P.listen(function(rq,rs0){
     })
   }
 })
+
+P.listen(8181)
 
 
 

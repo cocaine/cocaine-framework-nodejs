8ed307664d020a3b0db4cd5ae7a12e0ea7bc943e
Dmitry Unkovsky <oil.crayons@gmail.com>
2013-12-03 19:30:59 +0400

    introducing new fsm-based client, capable of human-consumable error handling and therefore reconnects

---
+++ sample/client.js
@@ -0,0 +1,42 @@
+
+
+var cli = new (require('../lib/client').Client)()
+
+
+cli.resolve('node', function(err, result){
+  console.log('resolve done:', result)
+})
+
+var baseTimeout = 500
+var timeout = baseTimeout
+
+var maxTries = 8
+var tries = 0
+
+cli.on('connect', function(){
+  timeout = baseTimeout
+  tries = 0
+})
+
+cli.on('error', function(err){
+  if(Array.isArray(err)){
+    if(err[0] === 'connect'){
+      console.log("connect fail. will we retry?")
+      if(tries++ < maxTries){
+        cli.setErrorHandler('retryConnect', [timeout])
+        timeout <<= 1
+      } else {
+        cli.emit('error', err[1])
+      }
+    } else if(err[0] === 'socket'){
+      console.log("fail on connected socket. will we reconnect?")
+      if(tries++ < maxTries){
+        timeout <<= 1
+        cli.setErrorHandler('reconnect', [timeout])
+      } else {
+        cli.emit('error', err[1])
+      }
+    }
+  }
+})
+

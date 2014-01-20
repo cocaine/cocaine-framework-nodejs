aa4ffaf2af21d145e426542e258c400e426743ed
Dmitry Unkovsky <oil.crayons@gmail.com>
2014-01-20 22:09:40 +0400

    housekeeping: add usefull traces; up jampack version

--- sample/urlfetch_client.js
+++ sample/urlfetch_client.js
@@ -4,14 +4,21 @@ var cli = new (require('../lib/client/client').Client)(['10.11.12.13', 10053])
 
 
 cli.getServices(['urlfetch'], function(err, urlfetch){
+
   if(err){
     console.log('error resolving some of services', err)
     return 
   }
 
   urlfetch.get('http://ya.ru', 3000, {}, {}, true, function(err, result){
-    console.log(arguments)
+    
+    console.log(result)
+    console.log(result[1].toString('utf8'))
+
   })
 
 })
+
+
+
 

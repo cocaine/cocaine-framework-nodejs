51fef3cc355501c7239f0a15987e6477cac1ed13
Dmitry Unkovsky <oil.crayons@gmail.com>
2014-07-23 17:17:31 +0400

    make use of ErrorEmitter in clients

--- sample/resolving.js
+++ sample/resolving.js
@@ -4,16 +4,56 @@ var Resolving = require('../lib/client/resolving').Resolving
 function smoke(){
 
   var Node = Resolving.def('node', {
-    locatorEndpoint: '10.11.12.13:10053'
+    stateless: true,
+    locator:{
+      endpoint: '10.11.12.13:10053'
+    }
   })
 
-  var node = new Node()
-    
-  setInterval(function(){
-    node.call('list', [], function(err, result){
-      console.log(arguments)
-    })
-  },1000)
+  var node = new Node({
+    maxConnectTries:1,
+    //maxReconnects:0,
+    locator:{
+      maxConnectTries:10,
+      //maxReconnects:0
+    }
+  })
+
+  node.connect()
+
+  node.on('connect', function(){
+    setInterval(function(){
+      node.list(function(err, result){
+        console.log('==== list result ====', arguments)
+      })
+    },3000)
+  })
+
+  // node.call('list', [], function(err, result){
+  //   console.log('==== list result ====', arguments)
+  // })
+
+  node.on('beforeError', function(err){
+
+    console.log('================ node beforeError', JSON.stringify(err))
+    if(err.condition === 'locator'){
+      if(err.code === 'ECONNREFUSED'){
+        // retry connect
+        this._setErrorHandler(['retry-all-requests'])
+        return true
+      }
+    } else if(err.code === 'ECONNREFUSED'){
+      this._setErrorHandler(['retry-connect'])
+      return true
+    }
+  })
+
+  // setInterval(function(){
+  //   node.call('list', [], function(err, result){
+  //     console.log('==== list result ====', arguments)
+  //   })
+  // },10000)
+
 }
 
 
--- sample/stateless.js
+++ sample/stateless.js
@@ -1,21 +1,39 @@
 
 var Stateless = require('../lib/client/stateless').Stateless
+var Locator = require('../lib/client/locator').Locator
+
 
 function smoke(){
 
-  var Locator = Stateless.def('locator', ['10.11.12.13:10053', 1, ['resolve', 'refresh']])
+  //var Locator = Stateless.def('locator', ['10.11.12.13:10053', 1, ['resolve', 'refresh']])
 
 
   var locator = new Locator({
-    maxConnectTries: 12
+    endpoint: '10.11.12.13:10053',
+    maxConnectTries: 10
   })
 
+  locator.on('beforeError', function(err){
+    console.log('================', JSON.stringify(err))
+    if(err.code === 'ECONNREFUSED'){
+      this._setErrorHandler(['retry-connect', err])
+      return true
+    }
+  })
+
+
+  locator.resolve('nodeasodifj', function(err, result){
+    if(err){
+      console.log('err.message', err.message)
+    }
+    console.log('resolve done', arguments)
+  })
 
   setInterval(function(){
     locator.call('resolve', ['node'], function(err, result){
       console.log(arguments)
     })
-  },1000)
+  },10000)
 }
 
 

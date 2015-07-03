b12b8da95c4f15a8726706311877926689bcd2c2
Dmitry Unkovsky <oil.crayons@gmail.com>
2015-07-03 17:06:02 +0300

    [test] prepare to test http streams

--- test/http.js
+++ test/http.js
@@ -169,43 +169,107 @@ describe('http worker', function(){
 
       return done()
 
-      var s = yield W.await('handle1')
-
-      wc.sendmsg([15, RPC.chunk, ['body']])
-
-      var d = yield s.await('data')
-
-      console.log('worker got data `%s`', d)
-
-      var m = yield wc.recvmsg()
-
-      console.log('response from worker, heartbeat', m)
+    }).catch(function(err){
+      process.nextTick(function(){
+        throw err
+      })
+    })
+    
+  })
 
 
-      var r = s.write(Buffer('r1'))
-      console.log('buffer consumed', r)
-      var r = s.write(Buffer('r2'))
-      console.log('buffer consumed', r)
+  it.only('should be able to stream output data', function(done){
 
-      var m = yield wc.recvmsg()
-      console.log('response from worker', m)
+    co(function *test(){
 
-      var m = yield wc.recvmsg()
-      console.log('response from worker', m)
+      debug('creating http server')
 
-      var r = s.end(Buffer('r3'))
-      console.log('buffer consumed', r)
+      var server = new http.Server(function(req, res){
+        
+        debug('http server got request')
+        
+        var body = []
 
-      var m = yield wc.recvmsg()
-      console.log('response from worker', m)
+        req.on('data', function(data){
+          debug('http server got data on request', data)
+          body.push(data)
+        })
+        
+        req.on('end', function(){
+          debug('end input stream, so writing response')
+          res.writeHead(200, {
+            'x-any-header': 'x-any-value',
+            'content-type': 'text/plain'
+          })
+          res.write('hello, Cocaine!')
+          res.write('hello, Cocaine!')
+          res.end('hello, Cocaine!')
+        })
+        
+      })
 
+      var handle = W.getListenHandle('http')
+
+      server.listen(handle)
+
+      var [_, wc] = yield [server.await('listening'), Node.await('connection')]
+
+      debug('http server listening, worker connected')
+
+      var m = yield wc.recvmsg(msgpack.unpack)
+
+      debug('node got message', m)
+
+      var [sid, method, [uuid]] = m
+
+      assert(uuid === options.uuid, "worker handshakes with it's uuid")
+
+      wc.sendmsg([15, RPC.invoke, ['http']])
+
+      var rq = ['GET','/','HTTP/1.1',
+             [['some-header','value'],
+              ['content-length', '7']],
+             'andbody']
+
+      wc.sendmsg([15, RPC.chunk, [msgpack.pack(rq)]])
 
       wc.sendmsg([15, RPC.choke, []])
 
-      yield s.await('end')
-      console.log('end stream from worker')
+      var m = yield wc.recvmsg()
 
-      done()
+      debug('node got message from worker', m)
+      
+      yield sleep(3*1000)
+
+      var m = yield wc.recvmsg()
+      debug('node got message from worker', m)
+      
+      var [method, sid, [chunk]] = m
+
+      debug('http response header is', msgpack.unpack(chunk))
+      
+
+      var m = yield wc.recvmsg()
+      debug('node got message from worker', m)
+      var [method, sid, [chunk]] = m
+      debug('actual http response is `%s`', chunk)
+
+      var m = yield wc.recvmsg()
+      debug('node got message from worker', m)
+      var [method, sid, [chunk]] = m
+      debug('actual http response is `%s`', chunk)
+
+      var m = yield wc.recvmsg()
+      debug('node got message from worker', m)
+      var [method, sid, [chunk]] = m
+      debug('actual http response is `%s`', chunk)
+      
+      yield sleep(3*1000)
+
+      var m = yield wc.recvmsg()
+      debug('node got message from worker', m)
+
+      return done()
 
     }).catch(function(err){
       process.nextTick(function(){
@@ -372,96 +436,6 @@ describe('http worker', function(){
     
   })
 
-
-  it.skip('should stream really huge chunks both ways', function(done){
-
-    co(function *test(){
-
-      W.connect()
-      var wc = yield Node.await('connection')
-
-      console.log('worker connected')
-
-      var m = yield wc.recvmsg(msgpack.unpack)
-
-      console.log('node got message', m)
-
-      var [method, sid, [uuid]] = m
-
-      assert(uuid === options.uuid, "worker handshakes with it's uuid")
-
-      wc.sendmsg([15, RPC.invoke, ['handle1']])
-
-      var s = yield W.await('handle1')
-
-      var b = Buffer(1024*1024*16)
-      
-      yield [
-        (function *sub1(){
-
-          for(var i=0;i<10;i++){
-            wc.sendmsg([15, RPC.chunk, [b]])
-            yield sleep(200)
-            
-            while(true){
-              var m = yield wc.recvmsg()
-              if(m[0] === 15){
-                // accept only messages from this channel id
-                break
-              }
-            }
-
-            var [sid, method, [buf]] = m
-
-            console.log('node got message back', [sid, method, [buf.slice(0,10)]], buf.length)
-
-            assert(buf.length === b.length, 'chunk should be of the same length as sent')
-            
-          }
-        })(),
-
-        (function *sub2(){
-          var i=0
-          while(i<10){
-            yield s.await('readable')
-
-            var d
-            while(d = s.read()){
-              i++
-              console.log('worker got data `%s`, length %d', d.slice(0, 30), d.length)
-              s.write(d)
-              console.log('writing data back')
-            }
-          }
-        })()]
-
-      // var m = yield wc.recvmsg()
-
-      // console.log('response from worker, heartbeat', m)
-
-      s.end()
-
-      wc.sendmsg([15, RPC.choke, []])
-
-      yield s.await('end')
-      console.log('worker: end of input stream')
-
-      var m = yield wc.recvmsg()
-      console.log('client: end of worker output stream', m)
-
-      assert.deepEqual(m, [15, RPC.choke, []])
-
-
-      done()
-
-
-    }).catch(function(err){
-      process.nextTick(function(){
-        throw err
-      })
-    })
-    
-  })
 
 })
 
